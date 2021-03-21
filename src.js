e6ef483f3911223469c9ac3002d38160bd353186
Obscuredc <77344064+Obscuredc@users.noreply.github.com>
2021-03-21 12:24:41 -0400

    updated [src.js]
    # updated [src.js]
    > added calculateNextFramePosition

--- src.js
+++ src.js
@@ -81,6 +81,125 @@ var nova = {
 			} else {
 				return  true;
 			}
+		},
+		getCollidedAngleForObjectA (objectA, objectB) {
+				var a = objectA.direction;
+			if (returnCollisionModeTypeIsHorizontalBoolean(objectA, objectB) == true) {
+				if (a == 360 || a == 0) {
+					return 0;	
+				} else if (a == 45) {
+					return 135;	
+				} else if (a == 90) {
+					return 90;	
+				} else if (a == 135) {
+					return 45;
+				} else if (a == 180) {
+					return 180;	
+				} else if (a == 225) {
+					return 315;	
+				} else if (a == 270) {
+					return 270;
+				} else if (a == 315) {
+					return 225;	
+				} else {
+					throw new Error("NovaJS getCollidedAngleError");	
+				}
+			} else if (returnCollisionModeTypeIsHorizontalBoolean(objectA, objectB) == false) {
+				if (a == 360 || a == 0) {
+					return 0;	
+				} else if (a == 45) {
+					return 315;	
+				} else if (a == 90) {
+					return 90;	
+				} else if (a == 135) {
+					return 225;
+				} else if (a == 180) {
+					return 180;	
+				} else if (a == 225) {
+					return 135;	
+				} else if (a == 270) {
+					return 270;
+				} else if (a == 315) {
+					return 45;	
+				} else {
+					throw new Error("NovaJS getCollidedAngleError");	
+				}
+			} else {
+				throw new Error("NovaJS getCollidedAngleError");	
+			}
+		},
+		calculateNextFramePosition (objectA, bool_forX) {
+			var ax = objectA.x;
+			var ay = objectA.y;
+			var s = objectA.speed;
+			var d = objectA.direction;
+			if (d == 0 || d == 360) {
+				if (bool_forX == true) {
+					return ax + s;
+				} else if (bool_forX == false) {
+					return ay + 0;	
+				} else {
+					throw new Error("NovaJS calculateNextFramePosition error");
+				}
+			} else if (d == 45) {
+				if (bool_forX == true) {
+					return s / 2 + ax;
+				} else if (bool_forX == false) {
+					return s / 2 + ay;	
+				} else {
+					throw new Error("NovaJS calculateNextFramePosition error");
+				}
+			} else if (d == 90) {
+				if (bool_forX == true) {
+					return ax + 0;
+				} else if (bool_forX == false) {
+					return ay + s;	
+				} else {
+					throw new Error("NovaJS calculateNextFramePosition error");
+				}
+			} else if (d == 135) {
+				if (bool_forX == true) {
+					return -1 * s / 2 + ax;
+				} else if (bool_forX == false) {
+					return s / 2 + ay;	
+				} else {
+					throw new Error("NovaJS calculateNextFramePosition error");
+				}
+			} else if (d == 180) {
+				if (bool_forX == true) {
+					return -1 * s + ax;
+				} else if (bool_forX == false) {
+					return ay + 0;	
+				} else {
+					throw new Error("NovaJS calculateNextFramePosition error");
+				}
+			} else if (d == 225) {
+				if (bool_forX == true) {
+					return -1 * s / 2 + ax;
+				} else if (bool_forX == false) {
+					return -1 * s / 2 + ay;	
+				} else {
+					throw new Error("NovaJS calculateNextFramePosition error");
+				}
+			} else if (d == 270) {
+				if (bool_forX == true) {
+					return ax + 0;
+				} else if (bool_forX == false) {
+					return -1 * s + ay;	
+				} else {
+					throw new Error("NovaJS calculateNextFramePosition error");
+				}
+			} else if (d == 315) {
+				if (bool_forX == true) {
+					return s / 2 + ax;
+				} else if (bool_forX == false) {
+					return -1 * s / 2 + ay;	
+				} else {
+					throw new Error("NovaJS calculateNextFramePosition error");
+				}
+			} else {
+				throw new Error("NovaJS incorrect angle applied to object cannot calculate");
+			}
 		}
 	}
 }
