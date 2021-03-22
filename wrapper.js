53c78b0bffbc4d1ef1fea506b7d3e78e0e66277f
Obscuredc <77344064+Obscuredc@users.noreply.github.com>
2021-03-22 19:18:30 -0400

    updated wrapper.js
    ! updated wrapper.js
    1 console.log message, 
    2 version variable

--- wrapper.js
+++ wrapper.js
@@ -1,3 +1,7 @@
+var __intNovaVersion__ = 0.0;
+var __NovaVersion__ = 'pre-alpha ' + __intNovaVersion__;
+console.log("novajs installed. Info:\n"+ __NovaVersion__);
+
 var nova = {
  'physics' : {
    
