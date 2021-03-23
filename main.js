79e62c28a9e31d014cb1a6743bbb2bfe66f583df
Obscuredc <77344064+Obscuredc@users.noreply.github.com>
2021-03-23 15:17:04 -0400

    renamed to 'main.js', updated 'main.js'
    ! renamed 'pipeline.js' to 'main.js'
    ! updated 'main.js'
    1 set up `start()` function
    2 set up `update()` function
    3 removed `loop()` function

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 var canvas = document.getElementById( >> CANVAS NAME HERE <<);
 var ctx = canvas.getContext('2d');
-function loop () {
+
   /**
     1. Clear Canvas
     2. draw
@@ -8,9 +8,16 @@ function loop () {
       > test for collisions using IsCollision
       > calculate angle and apply to object
     4. calculate for next frame using calculateNextFramePosition, set x and y to the positions
-    5. repeat until 'end' variable is true
-  
   **/
+
+function start () {
+  //this code will run on start
+  console.log("Hello, World!");
+  
+}
+
+function update () {
+  //this code will run every frame
   
   
 }
